1e545ec3ed878c53689c600cc4a15fa425173bb2
owengc99 <owencarruth@gmail.com>
2026-09-14 11:40:32 -0500

    ProductsofArrayExceptSelf.java

---
+++ neetcode-150/ProductsofArrayExceptSelf.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+        // Pass 1: output[i] = product of all elements to the left of i
+        output[0] = 1;
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        // Pass 2: multiply in the product of all elements to the right of i
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return output;
+    }
+}
